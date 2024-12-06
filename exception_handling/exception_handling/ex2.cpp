0bace9ab996e3140513179331b790bafd6a97500
Pavankumar-003 <amudalapavankumar003@gmail.com>
2024-12-06 18:59:25 +0530

    exception handling

---
+++ exception_handling/exception_handling/ex2.cpp
@@ -0,0 +1,27 @@
+///*try block without catch block */
+//
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a,b,c;
+//	a=10;
+//	b=0;
+////	cin>>a>>b;
+//	try
+//	{
+//		if(b==0)
+//			throw b;
+//		else
+//		{
+//			c=a/b;
+//			cout<<"divisible is "<<c<<endl;   
+//		}
+//	}
+//	catch(...)
+//	{
+//		cout<<"default catch"<<endl;
+//	}
+//	
+//}
---
+++ exception_handling/exception_handling/exp1.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a,b,c;
+//
+//	cin>>a>>b;
+//	try
+//	{
+//		if(b==0)
+//			throw b;
+//		else
+//		{
+//			c=a/b;
+//			cout<<"division is "<<c<<endl;
+//		}
+//	}
+//	catch(char x)
+//	{
+//		cout<<"divide by zero is not valid"<<endl;
+//		cout<<"b value is "<<x<<endl;
+//	}
+//	cout<<"after catch block"<<endl;
+//}
